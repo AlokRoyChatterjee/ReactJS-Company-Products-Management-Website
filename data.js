80a1052ccb5bed8db944882069a22ae3ca757602
Alok Roy Chatterjee <88044679+AlokRoyChatterjee@users.noreply.github.com>
2021-07-28 17:12:48 -0400

    Update data.js

--- data.js
+++ data.js
@@ -1,26 +1,23 @@
 export default [
   {
     id: 1,
-    title: 'Samsung Galaxy S7',
-    price: 599.99,
-    img:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1583368215/phone-2_ohtt5s.png',
+    title: 'Product 1',
+    price: 16,
+    img:'1.jpg',
     amount: 1,
   },
   {
     id: 2,
-    title: 'google pixel ',
-    price: 499.99,
-    img:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1583371867/phone-1_gvesln.png',
+    title: 'Product 2',
+    price: 10,
+    img: '2.jpg',
     amount: 1,
   },
   {
     id: 3,
-    title: 'Xiaomi Redmi Note 2',
-    price: 699.99,
-    img:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1583368224/phone-3_h2s6fo.png',
+    title: 'Product 3',
+    price: 16,
+    img: '3.jpg',
     amount: 1,
   },
 ];
