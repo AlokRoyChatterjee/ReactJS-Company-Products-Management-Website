7994bbabff0f81f35e10122b87221685fbc2b088
Alok Roy Chatterjee <88044679+AlokRoyChatterjee@users.noreply.github.com>
2021-07-28 17:23:17 -0400

    Update product.js

--- product.js
+++ product.js
@@ -1,20 +1,19 @@
 import React from 'react'
 import { useGlobalContext } from './context'
 const product = ({ id, img, title, price, amount }) => {
-  const { removeproducts, increaseproducts, decreaseproducts} = useGlobalContext()
+  const { takeawayproducts, increaseproducts, decreaseproducts} = useGlobalContext()
   return (
     <article className='cart-item'>
       <img src={img} alt={title} />
       <div>
         <h4>{title}</h4>
-        <h4 className='item-price'>${price}</h4>
-       
-        <button className='remove-btn' onClick={() => removeproduct(id)}>
-          Take away Item
+        <h4 >${price}</h4>
+        <button onClick={() => takeawayproducts(id)}>
+          take away product
         </button>
       </div>
       <div>
-      <p className='amount'>{amount}</p>
+      <p>{amount}</p>
        
       </div>
     </article>
