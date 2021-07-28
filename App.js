322b60ac46bce7fa86b8700648195d166a39a047
Alok Roy Chatterjee <88044679+AlokRoyChatterjee@users.noreply.github.com>
2021-07-28 17:28:41 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,24 +1,14 @@
 import React from 'react'
 import { useGlobalContext } from './context'
-
-// components
-import Navbar from './Navbar'
-import CartContainer from './CartContainer'
-// items
+import Productcomponent from './Productcomponent'
 
 function App() {
-  const { loading } = useGlobalContext()
-  if (loading) {
-    return (
-      <div className='loading'>
-        <h1>cart items...</h1>
-      </div>
-    )
-  }
   return (
     <main>
-      <Navbar />
-      <CartContainer />
+      <div>
+        <h1>Products Store</h1>
+      </div>
+      <Productcomponent />
     </main>
   )
 }
